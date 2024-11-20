e21dcece7a8515dae0d3a448f094c7d2455095b1
RafaelBG7 <rafaelborgespontes366@gmail.com>
2024-11-19 21:20:50 -0300

    ed

--- tests/tests_app.py
+++ tests/tests_app.py
@@ -1,4 +1,7 @@
-import app
+import json
+from app import app
 
-def test_dummy():
-    assert True
+def test_health_check():
+    client = app.test_client()
+    response = client.get('/')
+    assert response.status_code == 404  # Se não há rota '/', espera-se 404
