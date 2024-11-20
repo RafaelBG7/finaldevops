7bbb418c13ab24aa0988d741c231e6fe1ef3eb73
RafaelBG7 <rafaelborgespontes366@gmail.com>
2024-11-19 21:13:34 -0300

    agora vai

--- app.py
+++ app.py
@@ -1,15 +1,21 @@
 from flask import Flask, request, jsonify
 
+
 app = Flask(__name__)
+
 
 @app.route('/api/cadastrar', methods=['POST'])
 def cadastrar():
     dados = request.json
+
     if not dados:
         return jsonify({"error": "Dados inválidos"}), 400
-    
-    # Simulando o salvamento
-    return jsonify({"message": "Dados salvos com sucesso!", "dados": dados}), 201
+
+    return jsonify({
+        "message": "Dados salvos com sucesso!",
+        "dados": dados
+    }), 201
+
 
 if __name__ == '__main__':
     app.run(debug=True)
